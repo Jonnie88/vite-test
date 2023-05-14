366b99b869aebd38153012a641b173302e790ced
Johannes <Johanneslundberg88@gmail.com>
2023-05-14 20:22:17 +0200

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from "react";
+
 import Intro from "./components/Intro";
 import "./reset.css";
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(false);
+    }, 6000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
   return (
     <>
-      <Intro />
+      {isVisible ? <Intro /> : null}
+      {!isVisible ? <h1>Hello world</h1> : null}
     </>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Intro() {
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div className="intro">
-      <h1 className="slide-in-left">Hi👋 im Johannes</h1>
+      {isVisible ? <h1 className="slide-in-left">Hi👋 I'm Johannes</h1> : null}
+      {!isVisible ? (
+        <h2 className="slide-in-left"> An aspiring programmer 🤓</h2>
+      ) : null}
     </div>
   );
 }
